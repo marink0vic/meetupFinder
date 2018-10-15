3ddaca0fb8cb571ea17a3a117ea744d6750fe367
marko <salamanka1985@gmail.com>
2018-10-15 11:50:38 +0200

    Add method for handling Post request

--- src/main/java/com/meetupfinder/web/HomeController.java
+++ src/main/java/com/meetupfinder/web/HomeController.java
@@ -1,8 +1,18 @@
 package com.meetupfinder.web;
 
+import java.io.IOException;
+import java.net.URL;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Controller
 public class HomeController {
@@ -16,5 +26,57 @@ public class HomeController {
 		model.put("meetings", meetings);
 		meetings = null;
 		return "home";
+	}
+	
+	
+	@PostMapping("")
+	public String homePage(@ModelAttribute Location location, ModelMap model) throws JsonParseException, JsonMappingException, IOException {
+		ObjectMapper objectMapper = new ObjectMapper();
+		Location cityLoc = returnLocation(location, objectMapper);
+		
+		cityLoc.setTopic(location.getTopic());
+		System.out.println(cityLoc);
+		
+		meetings = returnMeetings(cityLoc, objectMapper);
+		if(meetings == null) {
+			model.put("message", "No topics matched your request");
+			return "home";
+		}
+		else if(meetings.length == 0) {
+			model.put("message", String.format("No %s meeting in %s", cityLoc.getTopic(),cityLoc.getCity()));
+			return "home";
+		}
+		
+		return "redirect:/";
+	}
+	
+	private Location returnLocation( Location location, ObjectMapper objectMapper) throws IOException {
+		String tempCity = location.getCity().replaceAll(" ", "+");
+		
+		String city = "https://api.meetup.com/2/cities?offset=0&query="+ tempCity 
+				+"&format=json&photo-host=public&page=10&radius=50&order=size&desc=false";
+		
+		URL urlClass = new URL(city);
+		JsonNode arrNode = new ObjectMapper().readTree(urlClass).get("results").get(0);
+		
+		return objectMapper.readValue(arrNode.toString(), Location.class);
+	}
+	
+	private Meetup[] returnMeetings(Location cityLoc, ObjectMapper objectMapper) throws JsonParseException, JsonMappingException, IOException{
+		
+		String meetingUrl = "https://api.meetup.com/2/groups?offset=0&format=json&lon="+cityLoc.getLon()+
+				"&topic="+cityLoc.getTopic()+"&photo-host=public&page=20&radius=25.0&fields=&lat="+cityLoc.getLat()+
+				"&order=id&desc=false&sig_id=246595295&sig=f6bda051287a4ffecf70cc6fefc70c1964827b09";
+		
+		URL urlClass = new URL(meetingUrl);
+		JsonNode arrNode = null;
+		
+		try {
+			arrNode = new ObjectMapper().readTree(urlClass).get("results");
+		} catch (IOException e) {
+			return null;
+		}
+		
+		return objectMapper.readValue(arrNode.toString(), Meetup[].class);
 	}
 }
