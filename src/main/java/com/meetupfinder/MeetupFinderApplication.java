596e9080f12b79172b83fce38a9eb34196192be5
marko <salamanka1985@gmail.com>
2018-10-15 11:22:06 +0200

    Initial commit

---
+++ src/main/java/com/meetupfinder/MeetupFinderApplication.java
@@ -0,0 +1,12 @@
+package com.meetupfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetupFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetupFinderApplication.class, args);
+	}
+}
